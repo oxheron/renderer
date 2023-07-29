75c266687c4985273f0b310933cfa6dc870cd991
0xheron <micahogden8@gmail.com>
2023-07-29 10:25:09 -0700

    Camera

--- src/world/camera.h
+++ src/world/camera.h
@@ -1,18 +1,20 @@
 #pragma once
 
 // external
+#include "glm/ext/matrix_transform.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
 
 // The basic camera
 // Has methods for moving around, however, more than that isn't implemented because it could depend on the type of application being developed
 class Camera
 {
 private:
-    glm::vec3 eye = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 eye = glm::vec3(0.0f, 0.0f, 1.0f);
     // The starting front position to rotated against
-    glm::vec3 start_front = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 front = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 start_front = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
     float pitch, roll, yaw;
@@ -31,5 +33,9 @@ public:
     void movey(float amount);
     void movez(float amount);
 
-    inline glm::mat4& get_view() { view = glm::lookAt(eye, eye + front, up); return view; }
+    glm::mat4 get_view() 
+    { 
+        view = glm::lookAt(eye, eye + front, up); 
+        return view;
+    }
 };
