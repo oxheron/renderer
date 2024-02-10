9bac883dd0d189c200178ce084480bdd3c875d9c
0xheron <micahogden8@gmail.com>
2024-02-10 14:30:12 -0800

    vertex layout stuff

--- src/definitions.h
+++ src/definitions.h
@@ -1,5 +1,25 @@
 #include <bgfx/bgfx.h>
 
+bgfx::VertexLayout pos_only_layout()
+{
+    static bgfx::VertexLayout layout;
+    static bool initialized = false;
+    if (!initialized)
+    {
+        layout.begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+            .end();
+        initialized = true;
+    }
+
+    return layout;
+}
+
+bgfx::VertexLayout objs_info_layout(uint8_t count)
+{
+    static bgfx::Attrib::Enum texcoords[] = 
+}
+
 bgfx::VertexLayout pos_tex_norm()
 {
     static bgfx::VertexLayout layout;
