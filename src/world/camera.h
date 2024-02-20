c77c5ef75f2154c758c6d50b49f8dda7f6b1fb9f
0xheron <micahogden8@gmail.com>
2024-02-19 16:57:08 -0800

    fix camera

--- src/world/camera.h
+++ src/world/camera.h
@@ -11,11 +11,11 @@
 class Camera
 {
 private:
-    glm::vec3 eye = glm::vec3(0.0f, 0.0f, 1.0f);
+    glm::vec3 eye = glm::vec3(0.0f, 0.0f, 0.0f);
     // The starting front position to rotated against
-    glm::vec3 start_front = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 start_front = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 front = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 up = glm::vec3(0.0f, 0.0f, 0.0f);
 
     float pitch, roll, yaw;
 
